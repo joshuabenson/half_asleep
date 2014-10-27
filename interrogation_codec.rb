dab92e670bdba5383210be58b7421144d1d35993
Josh Benson <bensonjosha@gmail.com>
2014-10-27 00:16:07 -0400

    added cool stuff

--- interrogation_codec.rb
+++ interrogation_codec.rb
@@ -9,3 +9,9 @@ state = gets.chomp
 
 puts "So let me get this straight.  They call you #{name_first}.  Not sure if thats your real name, sounds kinda made up."  
 puts "Now if I go down to #{city} and ask around about a #{name_first} #{name_last} are people going to look at me like I'm crazy?  Is #{city} even in #{state}?  You better not be yanking my chain here."
+
+puts "..."
+puts "What gender do you identify as?"
+gender_first = gets.chomp
+
+puts "#{gender_first.upcase} huh? Not my first guess.  I'll write that down here so that we'll be aware of that in the future."
