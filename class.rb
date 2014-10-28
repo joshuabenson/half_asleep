8f851ce4944b38b2a034a2e46ef64263b68b0206
Josh Benson <bensonjosha@gmail.com>
2014-10-28 00:23:03 -0400

    added animals classes to class.rb.  Interoggation has string slicing and is funnier.

--- class.rb
+++ class.rb
@@ -1,2 +1,48 @@
-50.%(6)
-Kernel.puts("Hello World")
+
+class Pet
+
+	def initialize(animal, weight_lbs, name, sound)
+		@weight = weight_lbs
+		@name = name
+		@animal = animal
+		@sound = sound
+
+	Kernel.puts("Who's a good #{@animal}?")
+
+	end
+
+	def speak
+		if @weight < 10
+		Kernel.puts("#{@sound.downcase}")
+	else
+		Kernel.puts("#{@sound.upcase}")
+end
+
+end
+
+
+	def feed(scoops)
+		if scoops.*(6) < @weight
+
+	scoops.to_s
+	Kernel.puts("#{@sound.upcase}! Only #{scoops} scoops? I'm a #{@weight.to_s}lb #{@animal}! You need to give me more food!")
+
+	scoops.to_i
+	@weight = @weight - ((@weight - scoops.*(6))/3) #seems that when this quotient thats subtracted drops below 1 it stops performing the operation
+													#rounding or refusal to subtract float from integer?
+													#it sortof makes sense for this 'game' though, an equilibrium point.  But dogs always beg anyway :)
+else
+
+	scoops.to_i	
+	@weight = @weight + (scoops./(2))
+	Kernel.puts("Chew Chew Chew Chew Chew #{@sound.downcase} Chew Chew")
+	end	
+end
+
+	
+end
+
+#	dog = pet.new
+# Kernel.puts(dog)
+
+
--- interrogation_codec.rb
+++ interrogation_codec.rb
@@ -7,11 +7,26 @@ city = gets.chomp
 print "Oh yeah? I forget, what state is that in?  Please use the abbreviation."
 state = gets.chomp
 
-puts "So let me get this straight.  They call you #{name_first}.  Not sure if thats your real name, sounds kinda made up."  
-puts "Now if I go down to #{city} and ask around about a #{name_first} #{name_last} are people going to look at me like I'm crazy?  Is #{city} even in #{state}?  You better not be yanking my chain here."
+puts "So let me get this straight.  They call you #{name_first.capitalize}.  Not sure if thats your real name, sounds kinda made up."  
+puts "Now if I go down to #{city.capitalize} and ask around about a #{name_first.capitalize} #{name_last.capitalize} are people going to look at me like I'm crazy?  Is #{city.capitalize} even in #{state.upcase}?  You better not be yanking my chain here."
 
 puts "..."
 puts "What gender do you identify as?"
 gender_first = gets.chomp
 
+gender_split = gender_first.split(//)	#splits the gender_split string into an array of each letter
+
 puts "#{gender_first.upcase} huh? Not my first guess.  I'll write that down here so that we'll be aware of that in the future."
+
+#gender_split[0] 
+
+print gender_split.delete_at(0)	#This is cool because it deletes the first index of gender_split but also prints it, very concise 
+
+
+gender_split.each do |x|	#iterates through and prints the rest of the array with a preceeding '-'
+	
+	print "-"+x
+	
+	end
+
+print "\n Interesting."    #\n starts newline, was having issue with that
